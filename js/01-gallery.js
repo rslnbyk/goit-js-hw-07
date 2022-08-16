d8d284939c6d00b7af549ed132df712fecf71372
rslnbyk <74673598+rslnbyk@users.noreply.github.com>
2022-08-16 15:19:32 +0300

    task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+let htmlGallery = "";
+let lightboxInstance;
+
+galleryItems.forEach((el) => {
+  htmlGallery += `<div class="gallery__item"><a class="gallery__link" href=${el.original}><img class="gallery__image" src=${el.preview} data-source=${el.original} alt=${el.description}/></a></div>`;
+});
+
+gallery.innerHTML = htmlGallery;
+
+gallery.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  lightboxInstance = basicLightbox.create(
+    `<img src=${e.target.getAttribute("data-source")}>`
+  );
+  lightboxInstance.show();
+});
+
+document.addEventListener("keydown", (e) => {
+  if (!basicLightbox.visible()) {
+    return;
+  }
+
+  if (e.key === "Escape") {
+    lightboxInstance.close();
+  }
+});
