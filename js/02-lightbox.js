93537de86dccda587234abf921bf0378b8454579
rslnbyk <74673598+rslnbyk@users.noreply.github.com>
2022-08-18 15:38:56 +0300

    task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,16 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+let htmlGallery = "";
+
+galleryItems.forEach((el) => {
+  htmlGallery += `<li><a class="gallery__item" href=${el.original}><img class="gallery__image" src=${el.preview} alt="${el.description}"/></a></li>`;
+});
+
+gallery.innerHTML = htmlGallery;
+
+const lightbox = new SimpleLightbox(`.gallery a`, {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
